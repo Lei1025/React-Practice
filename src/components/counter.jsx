baeecf9f95de670b0b6ca4789b4bb8906a227209
leiliu <llei1025@gmail.com>
2019-05-29 04:57:43 -0400

    updated

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,9 +4,9 @@ import { stat } from "fs";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
+    value: this.props.counter.value,
     //imageUrl: 'https://picsum.photos/200'
-    tags: ["tag1", "tag2", "tag3"]
+    //tags: ["tag1", "tag2", "tag3"]
   };
 
   /*
@@ -44,7 +44,7 @@ class Counter extends Component {
         
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increament</button>
-              
+        <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>     
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
 import Counters from './components/counters';
 
 ReactDOM.render(<Counters />, document.getElementById('root'));
